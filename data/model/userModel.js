e6d5b76c01f57e6922a5f4567c212c70a4301e70
egecan12 <egecan18k@gmail.com>
2024-02-28 18:46:15 -0500

    fixes css

--- data/model/userModel.js
+++ data/model/userModel.js
@@ -3,6 +3,7 @@ var mongoose = require("mongoose");
 var nameSchema = new mongoose.Schema({
   firstName: String,
   score: Number,
+  date: Date,
 });
 var User = mongoose.model("User", nameSchema);
 
